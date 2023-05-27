4f1609bdc8380645a976cda829abf7ee5d8e1126
GTNPLex <linuxQc@pm.me>
2023-05-27 14:00:48 +0200

    Css style

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,12 +1,28 @@
+
 import Link from 'next/link'
+import Image from 'next/image';
+import Head from 'next/head';
+import Layout from '../../components/Layout';
+
+const YourComponent = () => (
+  <Image
+    src="/images/profile.png" // Route of the image file
+    height={144} // Desired size with correct aspect ratio
+    width={144} // Desired size with correct aspect ratio
+    alt="Patricia"
+  />
+);
 
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
       <h1>First Post</h1>
       <h2>
-        <Link href="/">Back to home</Link>
+        <Link href="/">← Back to home</Link>
       </h2>
-    </>
-  )
+    </Layout>
+  );
 }
